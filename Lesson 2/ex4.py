50e21ef68b6f376920f9868eff5080359064edcf
kidn3ys <dyer.mitch@gmail.com>
2018-11-19 17:45:48 -0800

    ex4.py done

--- Lesson 2/ex4.py
+++ Lesson 2/ex4.py
@@ -1,13 +1,24 @@
 #!/usr/bin/env python
-###
-Read in the "show_ip_int_brief.txt" file into your program using the .readlines() method.
 
-Obtain the list entry associated with the FastEthernet4 interface. You can just hard-code the index at this point since we haven't covered for-loops or regular expressions. Use the string .split() method to obtain both the IP address and the corresponding interface associated with the IP.
+'''
+Read in the "show_ip_int_brief.txt" file into your program using the
+.readlines() method.
+
+Obtain the list entry associated with the FastEthernet4 interface. You can just
+hard-code the index at this point since we haven't covered for-loops or regular
+expressions. Use the string .split() method to obtain both the IP address and
+the corresponding interface associated with the IP.
 
 Create a two element tuple from the result (intf_name, ip_address).
 
 Print that tuple to the screen.
 
-Use pycodestyle on this script. Get the warnings/errors to zero. You might need to 'pip install pycodestyle' on your computer (you should be able to type this from the shell prompt). Alternatively, you can type 'python -m pip install pycodestyle'.
-###
-
+Use pycodestyle on this script. Get the warnings/errors to zero. You might need
+to 'pip install pycodestyle' on your computer (you should be able to type this
+        from the shell prompt). Alternatively, you can type 'python -m pip
+        install pycodestyle'.  '''
+with open("show_ip_int_brief.txt") as f:
+    show_brief = f.readlines()
+fa4 = show_brief[5].split()
+my_tuple = (fa4[0], fa4[1])
+print(my_tuple)
