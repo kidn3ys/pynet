e5ff5972ff6c3b1ad1455e77ea3c2e792847d833
kidn3ys <dyer.mitch@gmail.com>
2018-11-21 08:44:20 -0800

    lesson3 -ex3

--- Lesson 3/ex3.py
+++ Lesson 3/ex3.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#/usr/bin/env python
 '''
 Read the 'show_lldp_neighbors_detail.txt' file. Loop over the lines of this
 file. Keep reading the lines until you have encountered the remote "System Name"
@@ -8,4 +8,21 @@ your variables should only have 'twb-sf-hpsw1' and '15'). Break out of your loop
 once you have retrieved these two items.
 '''
 
+with open("show_lldp_neighbors_detail.txt") as f:
+	show_lldp = f.read()
 
+sys_name, port_id = '', ''
+
+for line in show_lldp.splitlines():
+	fields = line.split(":")
+	# Uncomment line below to validate loop breaks at the appropriate time
+	# print(line)
+	if "system name" in fields[0].lower():
+		sys_name = fields[1].strip()
+	if "port id" in fields[0].lower():
+		port_id = fields[1].strip()
+	
+	if sys_name and port_id:
+		print(sys_name + " / " + port_id)
+		break
+		
